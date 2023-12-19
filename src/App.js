8b09f2cb912b1ed3db76bb56068146fb6c6b068b
Ashish <ashishcoorg46@gmail.com>
2023-12-19 22:53:49 +0530

    changed nav

--- src/App.js
+++ src/App.js
@@ -11,25 +11,23 @@ import Search from './Pages/Search/Search';
 
 function App() {
   return (
-    <>
+
     <BrowserRouter>
-      <Header />
-     
-    <div className="app">
-    <Container>
-    <Routes>
-      <Route path="/" element={<Trending/>}/>
-      <Route path="/movies" element={<Movies/>}/>
-      <Route path="/series" element={<Series/>}/>
-      <Route path="/search" element={<Search/>}/>
-      </Routes>
-      </Container>
-   
-    </div>
-    <SimpleBottomNavigation style={{position:'fixed'}} />
-    
+      <>
+        <Header />
+        <div className="app">
+          <Container>
+            <Routes>
+              <Route path="/" element={<Trending />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/series" element={<Series />} />
+              <Route path="/search" element={<Search />} />
+            </Routes>
+          </Container>
+        </div>
+        <SimpleBottomNavigation style={{ position: 'fixed' }} />
+      </>
     </BrowserRouter>
-    </>
   );
 }
 
